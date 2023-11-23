e379a8d8c6258d9128c8bdc7761f60e935855b01
Ruxin Zheng <RuxinZ@users.noreply.github.com>
2023-11-23 15:30:57 -0800

    complete basic game functionality

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Guess-My-Number
+
+# Technical decisions
+
+1. Use Vite as Build tool
+2. Use Rest API
+3. Use backend Server to call the Random Generator API and forward the results to frontend client
+4. Error handling
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react';
 import './App.css';
 
+import Guess from './components/Guess';
+
 function App() {
   const [target, setTarget] = useState('');
+  const [numOfGuesses, setNumOfGuesses] = useState(10);
+
+  const updateNumOfGuesses = () => {
+    if (numOfGuesses == 1) console.log('You lost!');
+    setNumOfGuesses(numOfGuesses - 1);
+  };
+
   const generateRandomNum = async () => {
     try {
       const res = await fetch('api');
@@ -13,8 +22,18 @@ function App() {
   };
   return (
     <>
-      <button onClick={generateRandomNum}>Generate Number</button>
+      <button onClick={generateRandomNum}>Start Game</button>
       <p>Target: {target}</p>
+      <p>
+        {' '}
+        You have <strong>{numOfGuesses}</strong>{' '}
+        {numOfGuesses > 1 ? 'guesses' : 'guess'} left
+      </p>
+      <Guess
+        updateNumOfGuesses={updateNumOfGuesses}
+        target={target}
+        numOfGuesses={numOfGuesses}
+      />
     </>
   );
 }
