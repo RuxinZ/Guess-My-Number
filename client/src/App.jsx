6dc1835826c03d184ce3389d3029eea0796c61fa
Ruxin Zheng <RuxinZ@users.noreply.github.com>
2023-11-25 14:09:50 -0800

    feat:update gameStatus -display different view based on gameStatus

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,27 +2,40 @@ import { useState } from 'react';
 import './App.css';
 
 import Guess from './components/Guess';
+import Banner from './components/Banner';
+import NewGameBtn from './components/NewGameBtn';
 
 function App() {
   const [target, setTarget] = useState('');
   const [numOfGuesses, setNumOfGuesses] = useState(10);
+  const [gameStatus, setGameStatus] = useState('inGame'); // "inGame", "won", "lost", "loading", "error"
+  const [record, setRecord] = useState('');
+  const [tentativeGuess, setTentativeGuess] = useState('');
 
   const updateNumOfGuesses = () => {
     if (numOfGuesses == 1) console.log('You lost!');
     setNumOfGuesses(numOfGuesses - 1);
   };
 
-  const generateRandomNum = async () => {
+  const handleRestart = async () => {
+    setGameStatus('loading');
+
     try {
       const res = await fetch('api');
-      setTarget(await res.json());
+      const json = await res.json();
+      setGameStatus('inGame');
+      setTarget(json);
+      setNumOfGuesses(10);
+      setRecord('');
+      setTentativeGuess('');
     } catch (err) {
-      console.log(`Error generating number: ${err}`);
+      setGameStatus('error');
     }
   };
+
   return (
     <>
-      <button onClick={generateRandomNum}>Start Game</button>
+      <button onClick={handleRestart}>Start Game</button>
       <p>Target: {target}</p>
       <p>
         {' '}
@@ -33,7 +46,34 @@ function App() {
         updateNumOfGuesses={updateNumOfGuesses}
         target={target}
         numOfGuesses={numOfGuesses}
+        record={record}
+        setRecord={setRecord}
+        tentativeGuess={tentativeGuess}
+        setTentativeGuess={setTentativeGuess}
+        gameStatus={gameStatus}
       />
+      {gameStatus === 'won' && (
+        <Banner status="happy">
+          <p>
+            <strong>Congratulations!</strong> You got it in{' '}
+            <strong>
+              {11 - numOfGuesses === 1
+                ? '1 guess'
+                : `${11 - numOfGuesses} guesses`}
+            </strong>
+            .
+          </p>
+        </Banner>
+      )}
+      {gameStatus === 'lost' && (
+        <Banner status="sad" answer={target}>
+          <p>
+            Sorry, the correct answer is <strong>{target}</strong>.
+          </p>
+        </Banner>
+      )}
+      {gameStatus === 'error' && <p> Error generating game!</p>}
+      <NewGameBtn handleRestart={handleRestart} gameStatus={gameStatus} />
     </>
   );
 }
